6ae363ab56a756ebcc0f8b8c3d19d7ab82fda1c5
Isaac Maddox <isaacmaddox05@gmail.com>
2025-12-30 12:29:03 -0600

    Add Sentry reporting

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,27 @@ import { ListPage } from "./pages/ListPage";
 import { LoginPage } from "./pages/LoginPage";
 
 function App() {
-  return (
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <SettingsProvider>
-        <main className="bg-background max-w-full overflow-hidden px-4 mx-auto py-8 min-h-full grid grid-cols-1 place-items-center">
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<RequireAuth />}>
-                <Route index element={<HomePage />} />
-                <Route path="/l/:listId" element={<ListPage />} />
-              </Route>
-              <Route path="/" element={<AuthPage />}>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </main>
-        <Toaster closeButton theme="dark" richColors />
-      </SettingsProvider>
-    </ThemeProvider>
-  );
+   return (
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+         <SettingsProvider>
+            <main className="bg-background max-w-full overflow-hidden px-4 mx-auto py-8 min-h-full grid grid-cols-1 place-items-center">
+               <BrowserRouter>
+                  <Routes>
+                     <Route path="/" element={<RequireAuth />}>
+                        <Route index element={<HomePage />} />
+                        <Route path="/l/:listId" element={<ListPage />} />
+                     </Route>
+                     <Route path="/" element={<AuthPage />}>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                     </Route>
+                  </Routes>
+               </BrowserRouter>
+            </main>
+            <Toaster closeButton theme="dark" richColors />
+         </SettingsProvider>
+      </ThemeProvider>
+   );
 }
 
 export default App;
